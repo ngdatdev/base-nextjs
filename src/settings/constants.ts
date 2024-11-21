1d790a27ef4379c63cdcab803745bce77a52959d
ngdatdev <nvdatdz8b@gmail.com>
2024-11-21 23:57:58 +0700

    Init base

---
+++ src/settings/constants.ts
@@ -0,0 +1,23 @@
+const ACCESS_TOKEN = '_accessToken';
+const REFRESH_TOKEN = "_refreshToken";
+const USER_ID = "_sub";
+const ROLE = "_way";
+const USER_AVATAR = "_avt";
+const USER_FULLNAME = "_fullname";
+const EMAIL = "_email";
+
+const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+
+const constants = {
+    ACCESS_TOKEN,
+    REFRESH_TOKEN,
+    API_SERVER,
+    USER_ID,
+    ROLE,
+    USER_AVATAR,
+    USER_FULLNAME,
+    EMAIL
+}
+
+
+export default constants;
---
+++ src/style/themes/default/colors.ts
@@ -0,0 +1,22 @@
+const themeColors = {
+  primaryDarker: "#005D8D",
+  primaryDark: "#0284C7",
+  primary: "#0098ff",
+  primaryOpacity: "#0098ffbd",
+  primaryLight: "#1ca3ff",
+  primaryLighter: "#61bfff",
+
+  secondaryDarker: "#005D8D",
+  secondaryDark: "#0284C7",
+  secondary: "#CFE9FA",
+  secondaryLight: "#F7FCFE",
+  secondaryLighter: "#F6F8FB",
+
+  backgroundPrimary: "#FFF9F6",
+  backgroundSecondary: "#ffffff",
+
+  textNormal: "#000",
+  textWhite: "#FFF",
+};
+
+export default themeColors;
